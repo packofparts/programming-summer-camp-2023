9157c75b50a980e162c22ceec7e57b2dd2693e26
Teja Koripella <tejakoripella@gmail.com>
2023-08-24 14:58:00 -0700

    day 4 auto code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,54 +4,95 @@
 
 package frc.robot;
 
-import edu.wpi.first.hal.DriverStationJNI;
-import edu.wpi.first.util.WPIUtilJNI;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.RobotDriveBase;
-import edu.wpi.first.wpilibj.internal.DriverStationModeThread;
-import edu.wpi.first.wpilibj.romi.RomiGyro;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.Drive;
-import frc.robot.subsystems.Drivetrain;
-
-
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 
 /**
- * The VM is configured to automatically run this class. If you change the name of this class or the
- * package after creating this project, you must also update the build.gradle file in the project.
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
  */
 public class Robot extends TimedRobot {
-  
-  RomiGyro gyro = new RomiGyro();
-  Drivetrain drivetrain = new Drivetrain();
+  private final Spark _leftMotor = new Spark(0);
+  private final Spark _rightMotor = new Spark(1);
+  private final Timer _mTimer = new Timer();
 
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
   public void robotInit() {
-    drivetrain.setDefaultCommand(new Drive(drivetrain));
+
   }
 
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
   @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
+  public void robotPeriodic() {}
 
-  }
-
-  @Override
-  public void disabledInit() {
-      drivetrain.reset();
-  }
-
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
   @Override
   public void autonomousInit() {
-    
+    _mTimer.start();
   }
 
+  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    
+    if (_mTimer.get()<2){
+      _leftMotor.set(0.5);
+      _rightMotor.set(0.5);
+    } else if (_mTimer.get()<4){
+      _leftMotor.set(0.8);
+      _rightMotor.set(0.8);
+    } else{
+      _leftMotor.stopMotor();
+      _rightMotor.stopMotor();
+    }
   }
-  
 
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {
+    _mTimer.reset();
+  }
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
 }
